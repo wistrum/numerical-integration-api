5c69e9326d9dfe1262483c2e61e71c96228f57cb
Muhammad Bilal <thabowba@gmail.com>
2025-03-10 07:19:49 +0500

    added h2 dependency

--- src/test/java/com/wistrum/integrationapi/IntegrationapiApplicationTests.java
+++ src/test/java/com/wistrum/integrationapi/IntegrationapiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.wistrum.integrationapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class IntegrationapiApplicationTests {
 
 	@Test
