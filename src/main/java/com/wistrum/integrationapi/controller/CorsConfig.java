8e07557beb28aa1ae4a491b86180845f16473702
Muhammad Bilal <thabowba@gmail.com>
2025-03-10 17:13:43 +0500

    Edited CORS config(7), added CORS filter

--- src/main/java/com/wistrum/integrationapi/controller/CorsConfig.java
+++ src/main/java/com/wistrum/integrationapi/controller/CorsConfig.java
@@ -1,33 +1,28 @@
 package com.wistrum.integrationapi.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Configuration
 public class CorsConfig {
-
-    @Value("${cors.allowed-origins}")
-    private String allowedOrigins;
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         
-        config.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+        config.addAllowedOrigin("https://rococo-biscotti-18adad.netlify.app");
         
-        config.setAllowedMethods(List.of("POST", "OPTIONS", "GET", "PUT", "DELETE"));
-        config.setAllowedHeaders(List.of("Content-Type", "Authorization", "X-Requested-With", "Accept"));
-        config.setExposedHeaders(List.of("Authorization"));
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:8080");
+        
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
         config.setAllowCredentials(true);
-        config.setMaxAge(3600L);
-
+        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
